b4553ef6a8ab22bdb179ece94acf65cd4345c4c8
Markus-Amano <markus.a.amano@gmail.com>
2021-06-23 11:10:04 -0600

    fixed the black hole geodesics

--- src/SchwarzschildGeo.jl
+++ src/SchwarzschildGeo.jl
@@ -1,118 +1,152 @@
 module SchwarzschildGeo
 
-using DifferentialEquations, Images
+using DifferentialEquations, Images, ProgressMeter
 
 const black = RGB{N0f8}(0,0,0)
 const bh_image_file = joinpath(dirname(@__DIR__),"media","galaxy.jpg")
+const DEFAULT_TSPAN = (0.,10000.)
+const DEFAULT_R0 = 20.0
+const DEFAULT_MAX_R = 10000.
 
-function radial_U_diff!(du,u,p,ϕ)
-	du[1] = u[2]
-	du[2] = 3*(u[1])^2 - u[1]
-end
+f(r) = 1-1/r
+f′(r) = 1/r^2
 
-function initial_U_cond(θin, rin=20)
-	[
-	 1/rin,
-	 1/(rin*tan(θin))
-	 ]
-end
+function nullgeoeom!(dx,x,p,τ)
+	@debug "Parameter" τ
+	@debug "Coordinate Location" x
+	@debug "Parameters" p
 
-sch_f(r) = 1-2/r
+	r0 = p[1]
+	θ0 = p[2]
+	ṫ0 = p[3]
+	ϕ̇0 = p[4]
 
-L(ϕv, r0=20) = sin(ϕv)
-vr0(ϕv, r0=20, E=1) = -cos(ϕv)#-√(E^2*r0 - sch_f(r0)*sin(ϕv))
+	# x[1] = r, x[2] = ṙ, x[3] = θ, x[4] = θ̇, x[5] = t, x[6] = ϕ
+	r = x[1]
+	θ = x[3]
 
-function radial_R_diff!(dr,r,p,τ)
-	ℓ = L(p[1],p[2])
-	# r[2] ≡ ṙ
-	# r[1] ≡ r
-	dr[1] = r[2]
-	dr[2] = ℓ^2*(r[1]^-3. - 3*r[1]^-4.)
-	dr[3] = ℓ/(r[1])^2.0
-end
-
-function sch_ode_prob(ϕv; r0=20., tspan=(0.,10.))
-	ODEProblem(radial_R_diff!, [r0, vr0(ϕv, r0), ϕv], tspan, [ϕv,r0])
-end 
-
-function sch_ode_solve(ϕv; r0=20., tspan=(0.,10.))
-	condition(r,t,integrator) = r[1]>500 || r[1]<2
-	cb=DiscreteCallback(condition, terminate!)
-	solve(sch_ode_prob(ϕv; r0=r0, tspan=tspan), callback=cb)
-end 
-
-function tophi(ϕp,θp)
-	sqt = sqrt(cos(θp)^2 + sin(ϕp)^4)
-
-	cψ = sin(ϕp)^2/sqt
-	sψ = cos(θp)/sqt
-
-	return atan(cψ*sin(θp)*sin(ϕp) + sψ*cos(θp), sin(ϕp)*cos(ϕp)) + π, cψ, sψ
+	f0 = f(r0)
+	fr = f(r)
+	fp = f′(r)
 	
-	ϕp, 0, 0
+	ṫ = dx[5] = f0/fr*ṫ0
+	ϕ̇ = dx[6] = (r0*sin(θ0)/r/sin(θ))^2*ϕ̇0
+	θ̇ = dx[3] = x[4]
+	ṙ = dx[1] = x[2]
+	#=r̈=#dx[2] = (1/2)*(-fr*fp*ṫ^2 + fp/fr*ṙ^2 + 2*r*fr*(θ̇^2+sin(θ)^2*ϕ̇^2))
+	#=θ̈=#dx[4] = (1/2)*sin(2*θ)*ϕ̇^2 - 2*ṙ/r*θ̇
+	
+end
+
+# x⃗  = (r, θ, t, ϕ)
+# r⃗ᵥ = (ϕv, θv)
+function first_order_initial_from_view(x0, ang_view)
+	ϕv, θv = ang_view
+	r₀, θ₀ = x0
+	sqf₀ = sqrt(f(r₀))
+	return (-sqf₀*sin(θv)*cos(ϕv), cos(θv)/r₀, 1/sqf₀, sin(θv)*sin(ϕv)/r₀/sin(θ₀))
 end
 
 
-function fromphi(ϕ, cψ, sψ)
-	sψ *= -1
+# x⃗  = (r, θ, t, ϕ)
+# r⃗ᵥ = (ϕv, ϕv)
+function sch_ode_make(x0, ang_view; tspan=DEFAULT_TSPAN)
 
-	ϕp = atan(cψ*tan(ϕ),1)
-	θp = atan(sqrt(cos(ϕ)^2+cψ^2*sψ^2)/sψ*sin(ϕ)) + pi/2
+	# r0 = p[1]
+	# θ0 = p[2]
+	# ṫ0 = p[3]
+	# ϕ̇0 = p[4]
+	ẋ₀ = first_order_initial_from_view(x0, ang_view)
+	p = [x0[1], x0[2], ẋ₀[3], ẋ₀[4]]
 
-	return (ϕp, θp)
+	# x[1] = r, x[2] = ṙ, x[3] = θ, x[4] = θ̇, x[5] = t, x[6] = ϕ
+	x0dx0 = [x0[1], ẋ₀[1], x0[2], ẋ₀[2], x0[3], x0[4]]
+
+	return ODEProblem(nullgeoeom!, x0dx0, tspan, p)
 end
 
-function angletopix(ϕp, θp, imgdim)
-	x = mod(ϕp, 2*pi)/(2*pi)*imgdim[2] |> ceil |> Int
 
-	y = (pi - mod(θp, pi))/pi*imgdim[1]  |> ceil |> Int
+function sch_ode_solve(ϕv, θv; r0=DEFAULT_R0, θ0 = pi/2, t0=0.0, ϕ0=0.0, 
+											 tspan=DEFAULT_TSPAN, outerR = DEFAULT_MAX_R)
+	x0 = (r0, θ0, t0, ϕ0)
+	
+	ode = sch_ode_make(x0, (ϕv, θv); tspan)
+	
+	condition(x,t,integrator) = x[1] < 1 || x[1] > outerR
+	cb = DiscreteCallback(condition, terminate!)
 
-	y,x
+	sol = solve(ode, callback=cb)
+	return sol
+	
 end
 
-function schview(ϕin, θin; r0=20., tspan=(0.,100.))
-	# ϕ, cψ, sψ = tophi(ϕin, θin)
-	sol = sch_ode_solve(ϕin; r0=r0, tspan=tspan)
+function schview(ϕv, θv; r0=DEFAULT_R0, θ0 = pi/2, ϕ0=0.0, tspan=DEFAULT_TSPAN)
+	sol = sch_ode_solve(ϕv, θv; r0=r0, θ0=θ0, ϕ0=ϕ0, tspan=tspan)
 	
 	final = sol[end]
 
-	if final[1]<=2
+	if final[1]<=1
 		return :black, :black
 	end
 	
-	ϕsol = final[3]
-	ϕout, θout = ϕsol, θin
-	# ϕout, θout = fromphi(ϕsol, cψ, sψ)
+	ϕout, θout = final[[6,3]]
 
 	return ϕout, θout 
 end
 
-function schpic(imgfile=bh_image_file; r0 = 20.0, tspan=(0,100))
+function angletopix(ϕout, θout, imgdim)
+	y = mod(θout, 2*pi)
+	y = y < pi ? y : 2*pi-y
+	y = y/pi*imgdim[1]
+	yp = y |> ceil |> Int
+
+	xp = mod(ϕout, 2*pi)/(2*pi)*imgdim[2] |> ceil |> Int
+
+	if yp <= 0 
+		yp=1
+	elseif yp > imgdim[1]
+		yp=imgdim[1]
+	end
+
+	if xp == 0 
+		xp+=1
+	end
+
+	yp, xp
+end
+
+function schpic(imgfile=bh_image_file; r0 = DEFAULT_R0, tspan=DEFAULT_TSPAN, 
+		ϕcam=pi, θcam=0.0, ϕ0=0.0)
+
 	img = load(imgfile)
 	imgout = similar(img)
 
 	h,w = size(img)
-	
-	for j in 1:h, i in 1:w
-		# img[j,i]
-		ϕin = i/w*2*pi
-		θin = (j-h)/h*pi
+
+	@showprogress	for j in 1:h, i in 2:w-1
+		ϕin = i/w*2*pi 
+		θin = j/h*pi   
 		
-		ϕout, θout = schview(ϕin, θin; r0=r0, tspan=tspan)
+		if θin ≈ 0.0
+			θin += j/h*pi
+		elseif θin ≈ pi
+			θin -= j/h*pi
+		end
+
+		ϕout, θout = schview(ϕin - ϕcam, θin - θcam; r0=r0, tspan=tspan, ϕ0=ϕ0)
 
 		if ϕout == :black
 			imgout[j,i] = black
 			continue
 		end
 
-		viewpixind = angletopix(ϕout+pi, θout, (h,w))
-		
+		viewpixind = angletopix(ϕout, θout, (h,w))
 		imgout[j,i] = img[viewpixind...]
 	end
 
 	imgout
 end
 
-export radial_U_diff!, initial_U_cond, sch_ode_prob, schview, schpic
+export schview, schpic, sch_ode_make, sch_ode_solve, bh_image_file
 
 end # module
